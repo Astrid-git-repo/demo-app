06e2dbac59d9c143db0bc17f6edf8023c7b5eaa3
ASTRID DCOSTA <astdcost@publicisgroupe.net>
2022-08-26 00:22:59 -0400

    add

---
+++ src/main/java/com/springboot/demoAppSpringBoot/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.springboot.demoAppSpringBoot.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class DemoController {
+	Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+	@GetMapping("/welcome")
+	  String welcomeMessage() {
+	    return "Welcome!!!!";
+	  }
+	
+	@GetMapping("/welcome/{name}")
+	  public String welcomeMessage(@PathVariable String name) {
+		logger.info("Welcome Message --query param="+name);
+	    return "Hello There! Welcome "+name;
+	  }
+	
+	
+}
